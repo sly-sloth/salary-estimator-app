9f81bb523d14af541aa85208ad5052a02234b24e
Vaibhav <97592401+sly-sloth@users.noreply.github.com>
2024-06-17 05:18:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+st.set_page_config("Dev Salary Predictor")
 from predict_page import show_predict_page
 from explore_page import show_explore_page
 from about_page import show_about_page
