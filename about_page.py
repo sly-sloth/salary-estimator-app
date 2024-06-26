848eac7e3320a4759ce54bb70b7d52532c70955d
Vaibhav <97592401+sly-sloth@users.noreply.github.com>
2024-06-27 04:28:17 +0530

    Update about_page.py

--- about_page.py
+++ about_page.py
@@ -22,9 +22,7 @@ def show_about_page():
                 </style>
 
                 <span class="big-font">
-                The app utilizes the power of machine learning in order to predict the salary of a Software Developer given various parameters such as experience, education etc. and then calculates the salary based on those parameters. 
-                <br>
-                Finally, outputs the estimated result.
+                The app utilizes the power of machine learning in order to predict the salary of a Software Developer given various parameters such as experience, education etc. and then calculates the salary based on those parameters. Finally, outputs the estimated result.
                 <br>
                 <br>
                 The model behind it is trained on the Stack Overflow Developer Survey 2023, hence the latest data. The model also converts the salary to native currency using real time exchange rates.
